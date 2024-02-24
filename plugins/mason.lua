21eb143b6448a6194f0199699c79fd25e83a9410
wuchuheng <root@wuchuheng.com>
2024-02-24 21:54:36 +0800

    chore: update #2

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,6 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "lua_ls",
+          "rust-analyzer",
       })
     end,
   },
@@ -31,7 +32,6 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(
         opts.ensure_installed, {
           -- "python",
-          "rust-analyzer",
           "codelldb",
           "cpptools",
         }
