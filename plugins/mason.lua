cca88e61f8b5d968b3e3e7790e85e2f9efc0d61e
wuchuheng <root@wuchuheng.com>
2024-02-24 21:44:24 +0800

    chore: update #1

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -31,8 +31,8 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(
         opts.ensure_installed, {
           -- "python",
-          "codelldb",
           "rust-analyzer",
+          "codelldb",
           "cpptools",
         }
       )
